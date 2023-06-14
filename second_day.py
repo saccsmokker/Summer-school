73576f42b504ac4c6047b8384e12869fb9842057
saccsokker <toneesworld@outlook.com>
2023-06-14 00:11:42 -0700

    The inital stages

---
+++ second_day.py
@@ -0,0 +1,9 @@
+#temperature celsius to fahrenheit formula//
+
+def main ():
+    print ("Welcome to the Celsius to Fahrenheit converter ")
+    x = eval (input (" PLease enter the degrees in Celsius: "))
+    y = 32
+    print ((x * 1.8) + 32)
+main()
+
